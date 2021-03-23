93a16752c12a889e1116b82d5b164c12ed36e199
Terry Moore <tmm@mcci.com>
2021-03-23 11:55:41 -0400

    Add production logic to bootloader main

--- src/mccibootloader_main.c
+++ src/mccibootloader_main.c
@@ -85,13 +85,56 @@ Implementation notes:
         functions that shoudl have no hardware dependency. Those
         modules should not #include anything that is STM32L0-
         specific. The outer drivers are allowed to do STM32L0-
-        specific things.  As usual at MCCI, we avoid #if, but
-        we allow the outer drivers to use #if as needed, to 
-        minimize code footprint.
+        specific things.  As usual at MCCI, we avoid #if.
 
-        Functions named McciBootloaderPlatform_* are expected
-        to map (possibly via static inline) to code that is
-        specific to the targeted platform. 
+        Functions named McciBootloaderPlatform_* map (via static
+        inline) to code that is specific to the targeted platform.
+        The MCCI implementation uses our usual style of function
+        pointers and drivers; but the table is static.
+
+        It takes a long time to verify a ed25519 signature on the STM32L0;
+        so we only check signatures when deciding whether to program
+        the flash. Otherwise we check the SHA512. The program format
+        still includes the signature, but we only check this when
+        deciding whether to apply it to the flash.
+
+        The sequence is as follows:
+
+        1. If the boardloader hash is not valid, we stop with a failure code.
+        2. Otherwise, there is a saved boot loader state. The state can be
+           either "check flash" or "go".  If state is "go", we compute the
+           hash on the application image; if it's valid, we launch it. If
+           not valid, we set the "check flash" flag, and mark the application
+           invalid by its first page.
+        3. "check flash" is set, so we ask the platform to enable the flash
+           driver. (This may bring up power to the flash chip, bring up the
+           SPI bus, etc.)  We also ask the driver to enable notifications
+           as this is going to take a while.
+        4. We read through the flash application image.
+           (Including hash and signature.) This involves reading 4k at a
+           time and doing the signature check.
+        5. If the flash app image is not valid, and the application image
+           is valid, we reset the "check flash" flag and launch the application.
+        6. If the flash image is valid, we program it into the app image,
+           and then check the hash. If valid, we reset the "check flash flag",
+           set the "new app flag", and launch the app.
+        7. If the flash app image is not valid, and the application image is
+           not valid, we repeat step 4 using the "safety app" image. If the
+           "safety app" is valid, we repeat step 6 with the safety app. If
+           that fails, we fail with an indication (as at step 1).
+
+        States we're trying to establish:
+
+        Case    Boot    App     Flag    Flash   Safe    State
+         (1)    NG      -       -       -       -       Halt with indication
+         (2)    OK      OK      N       -       -       Launch app
+         (3)    OK      OK      Y       NG      -       Launch app, clear flag
+         (4)    OK      OK      Y       OK      -       Load flash, clear flag & reevaluate
+                                                         (Power failure during flash will
+                                                          bring us up in some App NG state)
+         (5)    OK      NG      -       OK      -       Load flash, clear flag & reevaluate
+         (6)    OK      NG      -       NG      OK      Load safe flash, clear flag & and reevaluate
+         (7)    OK      NG      -       NG      OK      Halt with indication
 
 */
 
@@ -101,20 +144,69 @@ McciBootloader_main(void)
         /* run the platform entry code. This must be minimal, if it exists at all */
         McciBootloaderPlatform_entry();
 
-        /* our first job is to check the signature of the boot loader */
+        /* our first job is to check the hash of the boot loader */
         if (! McciBootloader_checkCodeValid(&gk_McciBootloader_BootBase, McciBootloader_codeSize(&gk_McciBootloader_BootBase, &gk_McciBootloader_BootTop)))
                 {
-                /* boot loader isn't valid */
+                /* Case (1): boot loader isn't valid */
                 McciBootloaderPlatform_fail(McciBootloaderError_BootloaderNotValid);
                 }
 
-        /* next, we check the signature of the application */
-        if (! McciBootloader_checkCodeValid(&gk_McciBootloader_AppBase, McciBootloader_codeSize(&gk_McciBootloader_AppBase, &gk_McciBootloader_AppTop)))
+        /* next, we check the hash of the application */
+        bool const appOk = McciBootloader_checkCodeValid(&gk_McciBootloader_AppBase, McciBootloader_codeSize(&gk_McciBootloader_AppBase, &gk_McciBootloader_AppTop));
+
+        /* check the flash flag */
+        bool checkFlash;
+
+        checkFlash = McciBootloaderPlatform_getFlashFlag();
+        if (appOk && ! checkFlash)
                 {
-                /* application isn't valid -- we don't know how to read flash yet */
-                McciBootloaderPlatform_fail(McciBootloaderError_AppNotValid);
+                /* Case (2): looks like we're good to launch the application */
+                McciBootloaderPlatform_startApp(&gk_McciBootloader_AppBase);
                 }
 
-        /* looks like we're good to launch the application */
-        McciBootloaderPlatform_startApp(&gk_McciBootloader_AppBase);
+        /* initialize the flash and annunciator */
+        McciBootloaderPlatform_initializeFlash();
+        McciBootloaderPlatform_initializeAnnunciator();
+
+        /* check the app image */
+        do      {
+                /* don't check again unless asked */
+                McciBootloader_setFlashFlag(false);
+
+                McciBootloaderFlashAddress_t const hPrimary = McciBootloaderPlaform_getPrimaryFlashAddress();
+
+                const bool flashOk = McciBootloader_checkFlashImage(hPrimary);
+
+                /* check for Case (3) */
+                if (appOk && !flashOk)
+                        {
+                        /* case (3): looks like we're good to launch the application */
+                        McciBootloaderPlatform_startApp(&gk_McciBootloader_AppBase);
+                        }
+
+                /* case (4) or (5), maybe */
+                bool programOk;
+
+                programOk = McciBootloader_programAndCheckFlash(hPrimary);
+                if (programOk)
+                        {
+                        /* definitely (4) or (5): launch the application */
+                        McciBootloaderPlatform_startApp(&gk_McciBootloader_AppBase);
+                        }
+                } while (0);
+
+        /* case (6) or (7) */
+        do      {
+                McciBootloaderFlashAddress_t const hFallback = McciBootloaderPlaform_getFallbackFlashAddress();
+
+                const bool flashOk = McciBootloader_checkFlashImage(hFallback);
+                if (flashOk && McciBootloader_programAndCheckFlash(hFallback))
+                        {
+                        /* case (6) */
+                        McciBootloaderPlatform_startApp(&gk_McciBootloader_AppBase);
+                        }
+
+                /* case (7) */
+                McciBootloaderPlatform_fail(McciBootloaderError_NoAppImage);
+                } while (0);
         }
